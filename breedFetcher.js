0c57078b65952bf750c98a6ea98c46d30e06570f
RajMazing <caseyraj92@gmail.com>
2021-12-03 05:33:19 +0000

    updated and refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,19 @@
 const request = require("request");
 
-
-const fetchBreedDescription = function(breedName, callback) {
-let urlLink = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-request(urlLink, (err, response, body) => {
-if (!err) {
-const data = JSON.parse(body);
-if (body.length <= 2 || breedName !== data[0].name) {
-callback(breedName, null);
-} else {
-callback(null, data[0].description);
-}
-} else {
-callback(err);
-}
-});
+const fetchBreedDescription = function (breedName, callback) {
+  let urlLink = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(urlLink, (err, response, body) => {
+    if (!err) {
+      const data = JSON.parse(body);
+      if (body.length <= 2 || breedName !== data[0].name) {
+        callback(breedName, null);
+      } else {
+        callback(null, data[0].description);
+      }
+    } else {
+      callback(err);
+    }
+  });
 };
-
-
 
 module.exports = { fetchBreedDescription };
